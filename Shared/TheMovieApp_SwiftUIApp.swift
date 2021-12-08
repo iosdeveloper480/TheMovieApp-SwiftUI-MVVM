976d678b55264bcef47b9f37d0f7a0f36cb06a7e
Waqas Ali <waqasaliw@gmail.com>
2021-12-08 10:45:34 +0400

    Initial Commit

---
+++ Shared/TheMovieApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieApp_SwiftUIApp.swift
+//  Shared
+//
+//  Created by EDS on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
